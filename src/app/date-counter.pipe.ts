9d117b220bc892efb6d3675dee17d6e5c225f482
richardkefa <richardkefa@gmail.com>
2020-08-23 03:31:21 +0300

    add tranform function in pipe

--- src/app/date-counter.pipe.ts
+++ src/app/date-counter.pipe.ts
@@ -5,6 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCounterPipe implements PipeTransform {
 
-
+  transform(value: any):any{
+    let today: Date= new Date();
+    let todayWithNoTime:any= new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    let dateDifference=Math.abs(todayWithNoTime-value);
+    const secondsInDay=86400;
+    let dateDifferenceSeconds=dateDifference/1000;
+    let dateCount=dateDifferenceSeconds/secondsInDay;
+    if (dateCount>=1 && value<todayWithNoTime){
+      return dateCount;
+    }
+    else{
+      return "today"
+    }
+  }
 
 }
