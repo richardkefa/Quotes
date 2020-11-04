8d16ce77c34a6b42510a33f40f02f68695fcdb42
richardkefa <richardkefa@gmail.com>
2020-11-04 20:11:05 +0300

    backup

--- src/app/date-counter.pipe.ts
+++ src/app/date-counter.pipe.ts
@@ -9,14 +9,15 @@ export class DateCounterPipe implements PipeTransform {
 
   transform(value: any):any{
     console.log(value);
-    let today: Date= new Date();
-    console.log(today)
-    let todayWithNoTime:any= Date.UTC(today.getFullYear(),today.getMonth(),today.getDate())
-    console.log(todayWithNoTime);
-    let dateDifference=Math.floor(todayWithNoTime-value);
-    console.log(dateDifference)
-    if (dateDifference>=1 && value<todayWithNoTime){
-      return dateDifference;
+    let today: any= new Date();
+    console.log(today);
+    let dateDifference=Math.abs(value-today);
+    console.log(dateDifference);
+    var dateDifferenceSeconds=dateDifference/1000;
+    const secondsInDay=86400;
+    var daysPassed=Math.floor(dateDifferenceSeconds/secondsInDay);
+    if (dateDifference>=1 && value<today){
+      return daysPassed;
     }
     else{
       return "today"
